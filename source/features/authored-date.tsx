6c4f96f446019075e4634dc046538eb115db3992
Emily M Klassen <forivall@gmail.com>
2024-09-12 21:55:55 -0700

    chore(authored-date): fix lint issues

--- source/features/authored-date.tsx
+++ source/features/authored-date.tsx
@@ -1,4 +1,5 @@
 import * as pageDetect from 'github-url-detection';
+
 import features from '../feature-manager.js';
 import api from '../github-helpers/api.js';
 import observe from '../helpers/selector-observer.js';
@@ -14,31 +15,31 @@ function buildQuery(commits: string[]): string {
 				}
 			`).join('\n')}
 		}
-	`
+	`;
 }
 
 const getAuthorDates = async (commits: (string | undefined)[]): Promise<(string | undefined)[]> => {
-	const filteredCommits = commits.filter((commit): commit is string => !!commit && /^[0-9a-f]+$/i.test(commit))
-	if (!filteredCommits.length) {
-		console.log('warn: no commits found', commits)
-		return new Array(commits.length);
+	const filteredCommits = commits.filter((commit): commit is string => !!commit && /^[0-9a-f]+$/i.test(commit));
+	if (filteredCommits.length === 0) {
+		console.log('warn: no commits found', commits);
+		return Array.from({length: commits.length});
 	}
 	const {repository} = await api.v4(buildQuery(filteredCommits));
 
-	return commits.map((c) => c && repository[`_${c}`].authoredDate)
-}
+	return commits.map(c => c && repository[`_${c}`].authoredDate);
+};
 function getCommitHash(commit: HTMLElement): string | undefined {
-	const anchor = commit.querySelector('a[href*="/commit/"], a[href*="/commits/"]');
+	const anchor = commit.querySelector(['a[href*="/commit/"]', 'a[href*="/commits/"]']);
 	if (!anchor || !(pageDetect.isSingleCommit(anchor) || pageDetect.isPRCommit(anchor))) {
 		return;
 	}
 	return anchor.pathname.split('/').pop()!;
 }
-async function init() {
+async function init(): Promise<void> {
 	let commitElements: NodeListOf<HTMLElement> | Array<HTMLElement> = [];
 	let commitHashes: Array<string | undefined> = [];
 	if (pageDetect.isCommit()) {
-		commitElements = [document.querySelector('.commit-meta')!]
+		commitElements = [document.querySelector('.commit-meta')!];
 		commitHashes = [document.location.pathname.split('/').pop()!];
 	} else if (!pageDetect.isBlame()) {
 		commitElements = document.querySelectorAll([
@@ -46,25 +47,25 @@ async function init() {
 			'[data-testid="commit-row-item"]', // `isRepoCommitList`
 			'[data-test-selector="pr-timeline-commits-list"] .TimelineItem', // `isPRConversation`
 		].join(','));
-		commitHashes = new Array(commitElements.length);
-		commitElements.forEach((commit, i) =>{
-			commitHashes[i] = getCommitHash(commit)
-		})
+		commitHashes = Array.from({length: commitElements.length});
+		for (const [index, commit] of commitElements.entries()) {
+			commitHashes[index] = getCommitHash(commit);
+		}
 	}
 	const authoredDates = await getAuthorDates(commitHashes);
-	commitElements.forEach((commit, i) => {
-		const container =
-			commit.querySelector('[data-testid="listview-item-main-content"]') ||
-			commit.querySelector('.commit-author, [data-testid="author-avatar"]')?.parentElement;
+	for (const [index, commit] of commitElements.entries()) {
+		const container
+			= commit.querySelector('[data-testid="listview-item-main-content"]')
+			|| commit.querySelector(['.commit-author', '[data-testid="author-avatar"]'])?.parentElement;
 		if (container) {
 			const commitTime = container.querySelector('relative-time');
-			const authorDate = authoredDates[i];
+			const authorDate = authoredDates[index];
 			if (authorDate && commitTime?.getAttribute('datetime') !== authorDate) {
-				const relTime = document.createElement('relative-time');
-				relTime.setAttribute('datetime', authorDate);
+				const relativeTime = document.createElement('relative-time');
+				relativeTime.setAttribute('datetime', authorDate);
 				const span = document.createElement('span');
 				span.classList.add('color-fg-muted', 'pl-1');
-				span.append('(authored ', relTime, ')');
+				span.append('(authored ', relativeTime, ')');
 				if (commitTime) {
 					commitTime.after(' ', span);
 				} else {
@@ -72,22 +73,21 @@ async function init() {
 				}
 			}
 		}
-	})
+	}
 
 	if (pageDetect.isBlame()) {
-
+		// TODO
 	}
 };
-
 
 function hovercardInit(signal: AbortSignal): void {
 	observe('[data-hydro-view*="commit-hovercard-hover"]', updateHovercard, {signal});
 	observe('.react-blame-for-range', updateBlameLine, {signal});
 }
 
-async function updateBlameLine(container: HTMLElement) {
+async function updateBlameLine(container: HTMLElement): Promise<void> {
 	const commit = container.querySelector(
-		'[data-hovercard-url*="/commit/"]'
+		'[data-hovercard-url*="/commit/"]',
 	);
 	if (!commit) {
 		return;
@@ -101,12 +101,12 @@ async function updateBlameLine(container: HTMLElement) {
 	if (container) {
 		const commitTime = container.querySelector('relative-time');
 		if (authorDate && commitTime && commitTime.getAttribute('datetime') !== authorDate) {
-			const relTime = document.createElement('relative-time');
-			relTime.setAttribute('datetime', authorDate);
-			relTime.setAttribute('class', commitTime.getAttribute('class')!)
-			relTime.classList.add('color-fg-muted');
-			relTime.style.fontSize = 'xx-small';
-			commitTime.after(' ', relTime);
+			const relativeTime = document.createElement('relative-time');
+			relativeTime.setAttribute('datetime', authorDate);
+			relativeTime.setAttribute('class', commitTime.getAttribute('class')!);
+			relativeTime.classList.add('color-fg-muted');
+			relativeTime.style.fontSize = 'xx-small';
+			commitTime.after(' ', relativeTime);
 		}
 	}
 	// TODO: batch requests
@@ -127,9 +127,9 @@ async function updateHovercard(hovercardData: HTMLElement): Promise<void> {
 	if (!authorDate) {
 		return;
 	}
-	const relTime = document.createElement('relative-time');
-				relTime.setAttribute('datetime', authorDate);
-	authors[0].after(' ', relTime); // TODO: update text node too
+	const relativeTime = document.createElement('relative-time');
+	relativeTime.setAttribute('datetime', authorDate);
+	authors[0].after(' ', relativeTime); // TODO: update text node too
 }
 
 void features.add(import.meta.url, {
@@ -146,5 +146,5 @@ void features.add(import.meta.url, {
 	awaitDomReady: true, // TODO: start fetching based on page url
 	init,
 }, {
-	init: hovercardInit
+	init: hovercardInit,
 });
